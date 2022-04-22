c7681f76ba1c74148541a18d3928aa7928809467
Teem1345 <awogbuyitimothy@gmail.com>
2022-04-22 19:33:51 +0100

    Updates

---
+++ precache-manifest.78e28f25227dff24a18007544962c987.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d934ff23d66f59a336c0",
+    "url": "/41-Eatery/static/css/main.a2a125d9.chunk.css"
+  },
+  {
+    "revision": "d934ff23d66f59a336c0",
+    "url": "/41-Eatery/static/js/main.d934ff23.chunk.js"
+  },
+  {
+    "revision": "4b1b1bee0e8dc363b147",
+    "url": "/41-Eatery/static/js/1.4b1b1bee.chunk.js"
+  },
+  {
+    "revision": "959e037c3d9c888989f5",
+    "url": "/41-Eatery/static/js/runtime~main.959e037c.js"
+  },
+  {
+    "revision": "63766abef4f3baf8fb80c1e732873cc6",
+    "url": "/41-Eatery/static/media/dish.63766abe.png"
+  },
+  {
+    "revision": "7ca56790125ec552be8cf370b614604c",
+    "url": "/41-Eatery/static/media/snack.7ca56790.png"
+  },
+  {
+    "revision": "e6943336f02125d91e2d71b3d250a348",
+    "url": "/41-Eatery/static/media/vegetarian.e6943336.png"
+  },
+  {
+    "revision": "9dd8cfb194dc51a4019304d8c4035f2e",
+    "url": "/41-Eatery/static/media/customer-service.9dd8cfb1.png"
+  },
+  {
+    "revision": "1445033dcd42d670d0f7c65a578bfaf4",
+    "url": "/41-Eatery/static/media/about.1445033d.png"
+  },
+  {
+    "revision": "1d81b6ec991b03f6b25e4c70e04b6aaf",
+    "url": "/41-Eatery/static/media/meatball.1d81b6ec.jpg"
+  },
+  {
+    "revision": "6194da2c4f3c6b9bb4f71300cef90eab",
+    "url": "/41-Eatery/static/media/pizza.6194da2c.jpg"
+  },
+  {
+    "revision": "223cd405174463d27724aa8ad2bd0cf4",
+    "url": "/41-Eatery/static/media/spag.223cd405.jpg"
+  },
+  {
+    "revision": "741fb8adf5c0ad84dd786218b399758e",
+    "url": "/41-Eatery/index.html"
+  }
+];
