0b9b7a9551339c123f2b655200cb24ae51414bcb
Teem1345 <awogbuyitimothy@gmail.com>
2022-04-27 17:07:32 +0100

    implemented the responsiveness of all the pages for mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     return <div className='navbar-div'>
         <div className='container navbar'>
             <div className='logo'>
-                <Link to="/"><p>41-eatery</p></Link>
+                <Link to="/"><p>41-Eatery</p></Link>
             </div>
             <ul className='nav-links'>
                 <li><Link to='/'> Home </Link></li>
@@ -14,11 +14,15 @@ const Navbar = () => {
                 <li><Link to='/contact'> Contact</Link></li>
                 <li><Link to='/about'> About </Link></li>
             </ul>
-            <div>
+            <div className='bars-div'>
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
+            <div className='btn-div'>
                 <button className='btn-light'>Login</button>
                 <button className='btn-dark'>Sign Up</button>
             </div>
-       </div>
+        </div>
+        
     </div>
 }
 
