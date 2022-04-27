d608428eac38bfdd10bd570ea55de0c9da6f2c11
Teem1345 <awogbuyitimothy@gmail.com>
2022-04-27 18:41:05 +0100

    created the side bar and implemented it with JS

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={ <Home/> }/>
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,18 @@ import React from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
 
+
 const Navbar = () => {
+
+    const sideBar = document.getElementById('saide');
+    const showSide = () => {
+        sideBar.classList.add('active');
+    }
+    const closeSide = () => {
+        sideBar.classList.remove('active')
+    }
+
+
     return <div className='navbar-div'>
         <div className='container navbar'>
             <div className='logo'>
@@ -15,14 +26,24 @@ const Navbar = () => {
                 <li><Link to='/about'> About </Link></li>
             </ul>
             <div className='bars-div'>
-                <i class="fa fa-bars" aria-hidden="true"></i>
+                <h1 onClick={showSide}>=</h1>
             </div>
             <div className='btn-div'>
                 <button className='btn-light'>Login</button>
                 <button className='btn-dark'>Sign Up</button>
             </div>
         </div>
-        
+
+        {/* ------------------------------------------------------------------------sidebar */}
+          <div id="saide" className='side-bar'>
+            <h1 onClick={closeSide}>x</h1>
+            <ul className='side-links'>
+                <li><Link to='/'> Home </Link></li>
+                <li><Link to='/products'> Products </Link></li>
+                <li><Link to='/contact'> Contact</Link></li>
+                <li><Link to='/about'> About </Link></li>
+            </ul>
+      </div>
     </div>
 }
 
